76169f0680276c9bd20ef74990cdcf4884ea5a95
Ahmed <Ahmed.Emad@Mdi.com.eg>
2025-05-17 20:45:39 +0300

    new changes

--- src/main/java/CommonAssertions/CommonAssertions.java
+++ src/main/java/CommonAssertions/CommonAssertions.java
@@ -1,130 +1,16 @@
 package CommonAssertions;
 
-import Utilities.DriverManager;
 import Utilities.Actions;
 import org.junit.Assert;
 import org.openqa.selenium.By;
-//import org.assertj.core.api.Assertions;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.regex.Pattern;
 
 public class CommonAssertions {
-    public static void assertTwoStrings(String first, String second) {
-        Assert.assertEquals(first.replaceAll("\\s+",""), second.replaceAll("\\s+",""));
-    }
 
-    public static void stringMatching(String string){
-        assertTrue(Pattern.matches("^[a-zA-Z]+$",string));
 
-    }
-
-    public static void assertTwoIntegers(int first, int second) {
-        Assert.assertEquals(first, second);
-    }
 
     public static void assertElementDisplayed(By by) {
         Assert.assertTrue(Actions.elementDisplayed(by));
 
     }
-
-    public static void assertElementNotDisplayed(By by) {
-        Assert.assertTrue(Actions.elementNotExists(by));
-    }
-    public static void assertElementText(String expectedText,By by){
-        Assert.assertEquals(Actions.getElementText((by)).replaceAll("\\s+",""),expectedText.replaceAll("\\s+",""));
-    }
-
-    public static void assertElementExists(By by) {
-        Assert.assertTrue(Actions.elementExist(by));
-    }
-
-
-    public static boolean assertContains(String fullString, String actualtext) {
-        return fullString.replaceAll("\\s+","").contains(actualtext.replaceAll("\\s+",""));
-    }
-
-    public static void assertTrue(Boolean condition) {
-        Assert.assertTrue(condition);
-    }
-
-    public static void assertFalse(Boolean condition) {
-        Assert.assertFalse(condition);
-
-    }
-
-    public static boolean isNumber(String number) {
-        try {
-            Double.parseDouble(number.replaceAll("\\s", ""));
-        } catch (NumberFormatException nfe) {
-            return false;
-        }
-        return true;
-    }
-
-    public static boolean isValidCardNumber(String number) {
-        String[] groups = number.split(" ");
-        if (groups.length == 4) {
-            for (String group : groups) {
-                if (group.length() != 4 || !group.matches("\\d{4}")) {
-                    return false;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-
-    public static void isValidCVV(String number) {
-        CommonAssertions.assertTrue(number.length() == 3 && isNumber(number));
-    }
-
-    public static void validateCardExpiryDate(String expiryDate) {
-        CommonAssertions.assertTrue(expiryDate.matches("(?:0[1-9]|1[0-2])/[0-9]{2}"));
-    }
-
-    public static boolean validateOpeningDate(String date) {
-        DateFormat df = new SimpleDateFormat("dd MMM yyyy");
-        try {
-            df.parse(date);
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
-    }
-
-    public static void elementEnabled(By by) {
-        CommonAssertions.assertTrue(Actions.elementIsEnabled(by));
-    }
-
-    public static void elementDisabled(By by) {
-        CommonAssertions.assertFalse(Actions.elementIsEnabled(by));
-    }
-
-    public static void elementNotChecked(By by) {
-        CommonAssertions.assertFalse(Actions.elementIsChecked(by));
-    }
-
-    public static boolean hasCommaSeparator(String number) {
-        return number.matches("(\\d{1,3},)*\\d{1,3}(\\.\\d+)?");
-    }
-
-    public static boolean isTwoDecimalValues(String number) {
-        return number.matches("(\\d+,)*\\d*(\\.\\d{2})");
-    }
-    public static void firstAndLast(){
-
-    }
-
-    public static void assertElementEditable(By by) {
-        CommonAssertions.assertTrue(Actions.elementIsFocusable(by));
-    }
-    public static void assertTwoDoubles(double first, double second) {
-        Assert.assertEquals(first, second);
-    }
-
-
-
-
 }
--- src/main/java/Utilities/Actions.java
+++ src/main/java/Utilities/Actions.java
@@ -24,42 +24,7 @@ public class Actions extends DriverManager{
         return wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
 
-    public static void waitForInvisibilityOfElementLocatedBy(By by) {
-        WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), DEFAULT_WAIT_TIME);
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
-    }
 
-
-    //    public static void waitForElementInvisibilityOrNonClickability(By by) {
-//        WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), DEFAULT_WAIT_TIME);
-//
-//        // Try waiting for invisibility first (preferred if element should disappear)
-//        try {
-//            wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
-//            return; // If element becomes invisible, exit the function
-//        } catch (TimeoutException e) {
-//            // If invisibility wait times out, proceed to check non-clickability
-//        }
-//
-//        // If invisibility wait times out, wait for non-clickability as a fallback
-//        wait.until(ExpectedConditions.not(ExpectedConditions.elementToBeClickable(by)));
-//    }
-    public static void waitForElementDisappearance(By by) {
-        WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), DEFAULT_WAIT_TIME); // Change timeout to your desired seconds
-//        wait.until(ExpectedConditions.not(ExpectedConditions.elementToBeClickable(by)));
-        wait.until(ExpectedConditions.not(ExpectedConditions.elementToBeClickable(by)));
-
-    }
-    public static void selectFromListByText(String textToBeSelected) {
-        click(By.xpath("//android.widget.TextView[contains(@text,'" + textToBeSelected + "')]"));
-    }
-
-    public static WebElement waitForElementToContainText(By by, String awaitedText) {
-        WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), DEFAULT_WAIT_TIME);
-        wait.until(ExpectedConditions.presenceOfElementLocated(by));
-        wait.until(ExpectedConditions.textToBePresentInElementValue(by, awaitedText));
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-    }
 
     public static WebElement waitForElementClickable(By by) {
         WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), DEFAULT_WAIT_TIME);
@@ -77,44 +42,6 @@ public class Actions extends DriverManager{
     }
 
 
-
-    public static void doubleClick(By by) {
-        try {
-            waitForElementReadiness(by);
-
-            WebElement element = waitForElementClickable(by);
-            element.click();
-            element.click();
-        } catch (Exception e) {
-        }
-    }
-
-    public static boolean elementExists(By by) {
-        try {
-            WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), DEFAULT_WAIT_TIME);
-            wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-        } catch (Exception e) {
-            return false;
-        }
-        if (by != null) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean elementNotExists(By by) {
-        try {
-            WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), DEFAULT_WAIT_TIME);
-            wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
-        } catch (Exception e) {
-            return true;
-        }
-        if (by != null) {
-            return false;
-        }
-        return false;
-    }
-
     public static void type(String text, By by) {
         waitForElementReadiness(by);
         WebElement element = waitForElementClickable(by);
@@ -129,34 +56,13 @@ public class Actions extends DriverManager{
         return element.getText();
     }
 
-    public static boolean verifyElementTextEquals(By by, String text) {
-        waitForElementReadiness(by);
-        return DriverManager.getDriver().findElement(by).getText().equals(text);
-    }
-
-    public static boolean isElementNotVisible(By by) {
-        return DriverManager.getDriver().findElements(by).size() == 0;
-    }
-
-    public static void elementIsDisplayed(By by) {
-        DriverManager.getDriver().findElement(by).isDisplayed();
-    }
-
-    public static String getElementAttribute(By by, String Attribute) {
-        WebElement element = waitForElementReadiness(by);
-        return element.getAttribute(Attribute);
-
-    }
 
     public static boolean elementDisplayed(By by) {
         waitForElementReadiness(by);
         return driver.findElement(by).isDisplayed();
     }
 
-    //    public static boolean elementEnabled(By by){
-//        waitForElementReadiness(by);
-//        return DriverManager.getDriver().findElement(by).isEnabled();
-//    }
+
     public static boolean elementExist(By by) {
         if (waitForElementReadiness(by).isDisplayed()) {
             return true;
@@ -166,114 +72,7 @@ public class Actions extends DriverManager{
 
     }
 
-    public static void waitUntilLoadingFinish(By loadingIcon) {
-        while (true) {
-            try {
-                DriverManager.getDriver().findElement(loadingIcon).isDisplayed();
-            } catch (Exception e) {
-                break;
 
-
-            }
-        }
-    }
-
-    public static void wait(int timeInMilliseconds) {
-        try {
-            Thread.sleep(timeInMilliseconds);
-        } catch (InterruptedException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-
-    //TODO: implement later as touch action class is deprecated
-    public static void doubleTap(By by) {
-    }
-
-    public static void scrollToElement(By by) {
-    }
-
-    public static void longClickElement(By by) {
-
-    }
-
-    public static void dragElement(By by) {
-
-    }
-
-    public static boolean elementIsEnabled(By by) {
-        waitForElementReadiness(by);
-        return DriverManager.getDriver().findElement(by).isEnabled();
-    }
-
-    public static boolean elementIsChecked(By by) {
-        waitForElementReadiness(by);
-        return DriverManager.getDriver().findElement(by).getAttribute("checked").equals("true");
-    }
-    public static boolean elementIsNotChecked(By by) {
-        waitForElementReadiness(by);
-        return DriverManager.getDriver().findElement(by).getAttribute("checked").equals("false");
-    }
-
-    public static boolean elementIsClickable(By by) {
-        waitForElementReadiness(by);
-        return DriverManager.getDriver().findElement(by).getAttribute("clickable").equals("true");
-    }
-    public static boolean elementIsNotEnabled(By by) {
-        waitForElementReadiness(by);
-        return DriverManager.getDriver().findElement(by).getAttribute("enabled").equals("false");
-    }
-
-    public static boolean elementIsScrollable(By by) {
-        waitForElementReadiness(by);
-        return DriverManager.getDriver().findElement(by).getAttribute("scrollable").equals("true");
-    }
-
-    public static boolean elementIsNotClickable(By by) {
-        waitForElementReadiness(by);
-        return DriverManager.getDriver().findElement(by).getAttribute("clickable").equals("false");
-    }
-
-    public static void clear(By by) {
-        DriverManager.getDriver().findElement(by).clear();
-    }
-
-    public static int getSize(By by) {
-        waitForElementReadiness(by);
-        List<WebElement> elementSize = DriverManager.getDriver().findElements(by);
-        return elementSize.size();
-    }
-
-    public static boolean elementIsFocusable(By by) {
-        waitForElementReadiness(by);
-        return DriverManager.getDriver().findElement(by).getAttribute("focusable").equals("true");
-    }
-
-    public void fillElement(By element,String value)
-    {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.elementToBeClickable(element));
-        driver.findElement(element).sendKeys(value);
-    }
-    public void clickButton(By button)
-    { WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.elementToBeClickable(button));
-        driver.findElement(button).click();
-    }
-    public void selectElement(By element,int index)
-    {
-        WebElement Lists= driver.findElement(element);
-        Select selectFromTitles = new Select(Lists);
-        selectFromTitles.selectByIndex(index);
-    }
-    public String printText(By element)
-    {
-        return driver.findElement(element).getText();
-    }
-    public void scrollToElement(WebElement element) {
-        JavascriptExecutor js = (JavascriptExecutor) DriverManager.getDriver();
-        js.executeScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element);
-    }
 
     public static void scrollToEachElement(List<WebElement> elements) {
         JavascriptExecutor js = (JavascriptExecutor) DriverManager.getDriver();
